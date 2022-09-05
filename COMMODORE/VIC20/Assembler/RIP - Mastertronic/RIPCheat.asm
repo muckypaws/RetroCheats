24f37543bae990c5e1a64795f94f8c7037c8d041
muckypaws <jason@muckypaws.com>
2022-09-05 18:17:01 +0100

    RIP Mastertronic VIC20 Infinite Lives Cheat

---
+++ COMMODORE/VIC20/Assembler/RIP - Mastertronic/RIPCheat.asm
@@ -0,0 +1,54 @@
+; Start the Initial Program at $1900
+*=$1900
+     JSR     loaderCode; Load First File
+     LDA     #$20
+     STA     loaderRet
+     LDA     #$4c      ; Intercept Next Load Code
+     STA     $1684
+     lda     #<Intercept1
+     STA     $1685
+     lda     #>Intercept1
+     STA     $1686
+SetCommand
+     LDX     #$00      ; Set Index to Zero
+SetCommandLoop
+     LDA     INITIALLOAD,x
+     beq     SetCommandExit
+     STA     $277,x    ; Store in command Buffer
+     inx
+     bne     SetCommandLoop
+SetCommandExit
+     STX     $C6
+     rts               ; Now Run the Initial Code
+INITIALLOAD text    "run"
+     byte    13,0
+;
+; Start of the intercept code for the game.
+Intercept1
+     LDX     #$00
+InterceptRelocateLoop
+     LDA     pokeCode,x
+     STA     $210,x
+     inx
+     TXA
+     CMP     #40
+     BNE     InterceptRelocateLoop
+InterceptCodeEnd
+     JMP     $210
+pokeCode
+loaderCode
+     LDX     #$01
+     LDY     #$FF
+     JSR     $FFBA
+     LDA     #$00
+     JSR     $FFBD
+     LDX     #$FF
+     LDY     #$FF
+loaderRet
+     JMP     $FFD5     ; Load the Main Loader to Memory
+; Infinite Time
+     LDA     #$EA
+     STA     $12e4     ; Infinite Lives
+     LDA     #$0b
+     STA     $10f4     ; 9 Lives
+     JMP     $1400     ; Game Execution Address
---
+++ COMMODORE/VIC20/RIP - Mastertronic/readme.md
@@ -0,0 +1,23 @@
+# R.I.P. Mastertronic VIC20 Cheat
+
+## Jason Brooks 4th September 2022 - www.muckypaws.com
+
+This cheat will allow you to select 9 lives or infinite lives to help on your quest.
+
+## Files
+
+`
+RIPMastertronicCheat.tap		<-- TAP File with the cheat preloaded.
+RIPMastertronicCheat.bas		<-- Listing Type infinite
+RIPScratchPad					<-- Ignore just my work space.
+`
+
+Either type the cheat into your VIC20/Emulator or load the tape with the cheat on.
+
+Swap the cassettes over with the RIP Game, fully rewound.
+
+Type
+
+`RUN`
+
+Press play and hey presto, cheat mode enabled!
