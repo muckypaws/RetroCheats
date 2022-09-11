f44679144bc75c68b332c7e4e37bbe2134f0209d
muckypaws <jason@muckypaws.com>
2022-09-12 00:34:28 +0100

    Catcha Snatcha and Frantic - Imagine - VIC20

--- COMMODORE/VIC20/Assembler/Bewitched/Cheat.asm
+++ COMMODORE/VIC20/Assembler/Bewitched/Cheat.asm
@@ -1,11 +1,22 @@
 *=$220
 ; Load the BASIC Program
+    LDA  #$05
+    LDX  #$01
+    LDY  #$FF
+    JSR  $FFBA
+    LDA  #$00
+    TAX
+    TAY
+    JSR  $FFBD
+    LDA  #$00
+    JSR  $FFD5
 Inject
     LDA #<Cheat
-    STA $2fc
+    STA $12fc
     LDA #>Cheat
-    STA $2fd
-    JMP $21A        ; Execute the original loader
+    STA $12fd
+    RTS
+;    JMP $21A        ; Execute the original loader
 Cheat
     LDA #$19
     STA $100C
